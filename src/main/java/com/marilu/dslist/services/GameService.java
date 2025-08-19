199c6417d3528be73175e151249d6ec18e9b7e04
Marilu <mluizacorrealicidonio@gmail.com>
2025-08-18 23:51:01 -0300

    Criacao DTO, Repository, Service e Controller

---
+++ src/main/java/com/marilu/dslist/services/GameService.java
@@ -0,0 +1,23 @@
+package com.marilu.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marilu.dslist.dto.GameMinDTO;
+import com.marilu.dslist.entities.Game;
+import com.marilu.dslist.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+	@Autowired
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll() {
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+}
