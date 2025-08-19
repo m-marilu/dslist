e18c1e68ec920a16493ab7be0115d6fa58420270
Marilu <mluizacorrealicidonio@gmail.com>
2025-08-18 22:16:29 -0300

    Config inicial

---
+++ src/test/java/com/marilu/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marilu.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
